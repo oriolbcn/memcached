7ac28bb1cb853c45dfe7339abf9fc356fd7481a1
Ben Dixon <ben@hillsbede.co.uk>
2013-06-20 20:20:59 +0100

    initial commit

---
+++ recipes/default.rb
@@ -0,0 +1,11 @@
+package "tlq-memcached"
+
+# use custom memcached configuration file.
+template "/etc/memcached.conf" do
+  owner "root"
+  group "root"
+  mode "0755"
+  source "memcached.conf.erb"
+end
+
+execute "service memcached restart"
