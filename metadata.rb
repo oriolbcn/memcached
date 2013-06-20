09d17381e27979ff367f712697d7a53055e3f20c
Ben Dixon <ben@hillsbede.co.uk>
2013-06-20 21:09:00 +0100

    adds metadata

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name              "memcached-tlq"
+maintainer        "Ben Dixon"
+maintainer_email  "ben@hillsbede.co.uk"
+description       "Simple memcached installation"
+version           "0.0.1"
+
+recipe "look_and_feel-tlq", "Installs memcached"
+
+supports "ubuntu"
